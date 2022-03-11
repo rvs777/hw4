95c3754557280f2b342b42a5179772144d7eddc4
VenkataSudhakar(VESU) Rayaprolu <vesu.rayaprolu@kellogg.northwestern.edu>
2022-03-11 03:17:10 +0000

    WIP - My Changes 2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,21 +5,24 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by({ email: params["email"] })
     if @user
-      if @user.password == params["password"]
-        redirect_to "/places"
+      if BCrypt::Password.new(@user.password) == params["password"]
+        session[:user_id] = @user.id
+        #flash[:notice] = "Welcome, #{@user.username}."
+        redirect_to "/places" 
       else
-        flash[:notice] = "Nope"
+        flash[:notice] = "Login failed. Please try again."
         redirect_to "/sessions/new"
       end
     else
-      flash[:notice] = "Nope"
+      flash[:notice] = "Unknown User"
       redirect_to "/sessions/new"
     end
-    
   end
 
   def destroy
-
+    session[:user_id] = nil
+    flash[:notice] = "You are logged out now"
+    redirect_to "/sessions/new"
   end
 end
   
